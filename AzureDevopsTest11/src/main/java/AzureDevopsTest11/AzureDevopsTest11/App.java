baf7f062c4b61a73dda4ef1847011153f15420e7
shwetha337 <shwethas230996@gmail.com>
2022-10-28 16:11:30 +0530

    1st commit

---
+++ AzureDevopsTest11/src/main/java/AzureDevopsTest11/AzureDevopsTest11/App.java
@@ -0,0 +1,13 @@
+package AzureDevopsTest11.AzureDevopsTest11;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
